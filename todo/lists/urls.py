759cfde7d4d7eade233e478a9b965c67f577b26b
GK <gabrys.konarski@wp.pl>
2022-12-05 21:20:33 +0100

    added account page

--- todo/lists/urls.py
+++ todo/lists/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.lists, name="lists"),
     path("create/", views.addList, name="create"),
+    path("account/", views.userDetail, name="account"),
+    path("view/<str:pk>/", views.viewList, name="view"),
     path("delete-list/<str:pk>/", views.deleteList, name="delete-list"),
     path("delete-item/<str:pk>/", views.deleteItem, name="delete-item"),
-    path("view/<str:pk>/", views.viewList, name="view"),
+    path("delete-category/<str:pk>/", views.deleteCategory, name="delete-category"),
 ]
